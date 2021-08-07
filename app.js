8941fb3d9029717ac0acedaeef282d69ee0768fa
YellowFlash2012 <cyclop9@hotmail.com>
2021-08-07 16:45:29 +0100

    Add index.html style.css app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const mobMenu = document.querySelector('.nav-links');
 
 const darkLightTheme = document.querySelector('.dark-light');
 
+const caretUp = document.querySelector('.fa-angle-up');
+const caretDown = document.querySelector('.fa-angle-down');
+
+const subMenu = document.querySelector('.sub-menu');
+
 //open/close mobile menu
 mobMenuIcon.addEventListener('click', () => {
     
@@ -13,4 +18,20 @@ mobMenuIcon.addEventListener('click', () => {
 //toggle light/dark theme
 darkLightTheme.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+})
+
+//open/close submenu with caret
+caretDown.addEventListener('click', () => {
+    subMenu.style.display = 'block';
+    
+    caretDown.innerHTML = `<i hidden class="fas fa-angle-up"></i>`;
+
+    caretUp.style.display = 'block';
+})
+
+caretUp.addEventListener('click', () => {
+    subMenu.style.display = 'none';
+
+    caretUp.innerHTML = `<i hidden class="fas fa-angle-down"></i>`;
+  
 })
